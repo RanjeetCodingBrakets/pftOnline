276b2961775dc89a3e9d1c0895620e846effbd1d
RanjeetCodingBrackets <design@codingbrackets.dev>
2024-06-21 14:02:34 +0530

    update added the matrial ui

---
+++ src/components/aboutUsComponent/aboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us Page</h1>
+      <p>Welcome to the About Us page!</p>
+    </div>
+  );
+}
+
+export default AboutUs;
